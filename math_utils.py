bbff66c07f26d9c13cdda5980e336b76025c3f92
Shaked178 <aesgbcr@gmail.com>
2025-10-30 00:20:21 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -15,6 +15,6 @@ def find_mean_std(num1, num2, num3):
     n = 3
     mean = find_mean(num1, num2, num3)
     sum_of_squares = (num1 - mean) **2 + (num2 - mean) **2 + (num3 - mean) **2
-    std = math.sqrt(sum_of_squares / n)
+    std = (sum_of_squares / n) **0.5
     return mean, std
 
