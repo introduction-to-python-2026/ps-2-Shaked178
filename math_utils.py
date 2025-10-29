435e8b1d4bcf57d7779411d80d259da6deb95cf2
Shaked178 <aesgbcr@gmail.com>
2025-10-29 17:24:18 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,5 +1,9 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
+    if num1 > num2 and num2 >num3:
+      return num1
+    if num2 > num1 and num2 > num3:
+      return num2
+    return num3
 
 def find_mean(num1, num2, num3):
     pass  # Replace 'pass' with code
